2c4bef03f805cc790a07a441827cfd56a0ee25dd
Tiago Santos <tiago.s.1189@gmail.com>
2021-10-03 20:46:55 +0100

    adding amqpConsumerController to index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 
 import { services } from './services';
-import { expressController } from './utils/controller';
+import { 
+  amqpConsumerController,
+  expressController
+} from './utils/controller';
 
 const app = express();
 const port = 5000;
@@ -12,4 +15,6 @@ app.use('/webhook-providers', expressController(services.webhookProvidersService
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
+
+amqpConsumerController(services.webhookProvidersService);
